0c4ffb088cb8e46ef9f6b6382a188e466663c05d
Rajashekaran, Veera <Veera.Rajashekaran@pearson.com>
2016-06-06 07:17:07 +0530

    adding project code

---
+++ app/services/flash.service.js
@@ -0,0 +1,60 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('FlashService', FlashService);
+
+    FlashService.$inject = ['$rootScope', '$timeout'];
+    function FlashService($rootScope, $timeout) {
+        var service = {};
+
+        service.Success = Success;
+        service.Error = Error;
+
+        initService();
+
+        return service;
+
+        function initService() {
+            $rootScope.$on('$locationChangeStart', function () {
+                clearFlashMessage();
+            });
+        }
+
+        function clearFlashMessage() {
+            var flash = $rootScope.flash;
+            if (flash) {
+                if (!flash.keepAfterLocationChange) {
+                    delete $rootScope.flash;
+                } else {
+                    // only keep for a single location change
+                    flash.keepAfterLocationChange = false;
+                }
+            }
+        }
+
+        function Success(message, keepAfterLocationChange, timeout) {
+            $rootScope.flash = {
+                message: message,
+                type: 'success', 
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+            if(timeout){
+                $timeout(clearFlashMessage, timeout);
+            }            
+        }
+
+        function Error(message, keepAfterLocationChange, timeout) {
+            $rootScope.flash = {
+                message: message,
+                type: 'error',
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+            if(timeout){
+                $timeout(clearFlashMessage, timeout);
+            }
+        }
+    }
+
+})();
